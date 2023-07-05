80635966fadcf037131c9f54eed7b14a226a771a
Edward Hedenberg <edward@uniultra.xyz>
2023-07-05 10:29:31 +0700

    :rocket: Add Deployment file

--- fix.sh
+++ fix.sh
@@ -4,6 +4,3 @@ git filter-branch --env-filter '
   GIT_COMMITTER_NAME="Edward Hedenberg";
   GIT_COMMITTER_EMAIL="edward@uniultra.xyz";
 ' -- --all
-
-git filter-branch --index-filter 'git rm -rf --cached --ignore-unmatch ./fix.sh' HEAD
-git filter-branch --index-filter 'git rm -rf --cached --ignore-unmatch ./.gitlab-ci.yml' HEAD
